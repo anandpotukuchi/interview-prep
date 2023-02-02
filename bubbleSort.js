4d93626e9d3156fa43e1cc2e9afef7d8bb3b744e
Anand Potukuchi <anand@anandpotukuchi.me>
2023-02-02 14:02:33 +0530

    changed syntax of bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,19 +1,16 @@
 function bubbleSort(arr) {
-    // Only change code below this line
-    var temp;
-  for(var i=0;i<arr.length;i++) {
-      for (var j=0;j<arr.length-i-1;j++) {
-        if(arr[j] > arr[j+1]) {
-            temp=arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1]=temp;
-          }
-      }
+  // Only change code below this line
+for(var i=0;i<arr.length;i++) {
+    for (var j=0;j<arr.length-i-1;j++) {
+      if(arr[j] > arr[j+1]) {
+          ;[arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+        }
     }
-      console.log(arr)
-      //return arr;
-    
-    // Only change code above this line
   }
+    console.log(arr)
+    //return arr;
   
-  bubbleSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92])
+  // Only change code above this line
+}
+
+bubbleSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92])
